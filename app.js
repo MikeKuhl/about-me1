4282d689c708888bd9e68d4f560dd55fd3e7d020
Michael Metcalf <m.metcalf6453@gmail.com>
2021-10-26 15:01:17 -0700

    commented out console logs and replaced with alerts

--- app.js
+++ app.js
@@ -11,13 +11,14 @@
 // }
 
 // can I make this into booleen
-
 let userKnow = prompt('Do you know Michael Metcalf?','Yes or No');
 switch (userKnow.toLowerCase()) {
 case 'y':
+//   console.log('this user knows me');
   alert('Well its nice to know you.');
   break;
 case 'yes':
+//   console.log('this user knows me');
   alert('Well its nice to know you.');
   break;
 default:
@@ -27,9 +28,11 @@ default:
 let userLoc = prompt('Are you in the Seattle area?','Yes or No');
 switch (userLoc.toLowerCase()) {
 case 'y':
+//   console.log('this user is in Seattle');
   alert('Maybe we will run into eachother sometime.');
   break;
 case 'yes':
+//   console.log('this user is in Seattle');
   alert('Maybe we will run into eachother sometime.');
   break;
 default:
@@ -39,9 +42,11 @@ default:
 let userHS = prompt('Did you go to South Kitsap High School?','Yes or No');
 switch (userHS.toLowerCase()) {
 case 'y':
+//   console.log('this user went to SKHS');
   alert('Maybe we had a class together or just passed eachother in the halls.');
   break;
 case 'yes':
+//   console.log('this user went to SKHS');
   alert('Maybe we had a class together or just passed eachother in the halls.');
   break;
 default:
@@ -51,9 +56,11 @@ default:
 let userMil = prompt('Have you been in the military?','Yes or No');
 switch (userMil.toLowerCase()) {
 case 'y':
+//   console.log('this user was in the military');
   alert('Maybe we ran into eachother at some point.');
   break;
 case 'yes':
+//   console.log('this user was in the military');
   alert('Maybe we ran into eachother at some point.');
   break;
 default:
@@ -63,9 +70,11 @@ default:
 let userCF = prompt('Have you been in a Code Fellows course?','Yes or No');
 switch (userCF.toLowerCase()) {
 case 'y':
+//   console.log('this user went to Code Fellows');
   alert('Fellow Dev, Noice!');
   break;
 case 'yes':
+//   console.log('this user went to Code Fellows');
   alert('Fellow Dev, Noice!');
   break;
 default:
